148697117a473344ee003f0a80b30af270cbe590
ayushaggarwal600 <ayushaggarwal600@gmail.com>
2024-05-21 15:31:46 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   css: {
     postcss: {
       plugins: [tailwindcss()],
     },
-    plugins: [react()],
   },
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/_redirects",
+          dest: ".",
+        },
+      ],
+    }),
+  ],
   server: {
     proxy: {
       "/api/": "https://agayu.onrender.com",
