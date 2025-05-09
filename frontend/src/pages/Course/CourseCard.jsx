dab627066e8b93ebdaf6e1efcbf41e98f8239f50
ayushaggarwal600 <ayushaggarwal600@gmail.com>
2025-05-09 17:10:08 +0530

    Added some more dialogues

--- frontend/src/pages/Course/CourseCard.jsx
+++ frontend/src/pages/Course/CourseCard.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import CustomTab from "../../components/CustomTab";
 import HeartIcon from "../HeartIcon";
+import { useTranslation } from "react-i18next";
 
 const CourseCard = ({ course, condition }) => {
   if (!course.approvedByAdmin || !course.readyToPublished) return null;
   const navigate = useNavigate();
+  const { t } = useTranslation();
   return (
     <div>
       <div className="flex flex-col justify-between text-white mb-5 hover:scale-105 duration-150 ease-in-out">
@@ -23,15 +25,15 @@ const CourseCard = ({ course, condition }) => {
               <HeartIcon course={course} css={true} condition={condition} />
             </div>
             <div>
-              <h2 className="mt-1 mx-1">{course?.name}</h2>
-              <h3 className="mx-1">{course?.teacherName}</h3>
+              <h2 className="mt-1 mx-1">{t(`${course?.name}`)}</h2>
+              <h3 className="mx-1">{t(`${course?.teacherName}`)}</h3>
             </div>
           </div>
           <div className="flex justify-between items-center w-[20rem] gap-2 mt-1">
             <div className="ml-1">
               {course?.tags?.map((tag, i) => (
                 <CustomTab variant="outside" customCSS="mt-1" key={i}>
-                  {tag}
+                  {t(`${tag}`)}
                 </CustomTab>
               ))}
             </div>
